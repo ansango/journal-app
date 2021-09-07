93460689e74aaa3b54b2e8a69cf5371fc96a66b7
ansango <anibalsantosgo@gmail.com>
2021-09-07 17:12:36 +0200

    form register

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,16 +1,38 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
 
 const RegisterScreen = () => {
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({
+    name: "Anibal",
+    email: "ansango@ansango.com",
+    password: "1234",
+    password2: "1234",
+  });
+
+  const { name, email, password, password2 } = formValues;
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+
+    
+    //dispatch(startLoginEmailPassword(email, password));
+  };
+
   return (
     <>
       <h3 className="auth__title">Register</h3>
-      <form>
+      <form onSubmit={handleRegister}>
         <input
           type="text"
           placeholder="Name"
           name="name"
           className="auth__input"
           autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
         />
         <input
           type="text"
@@ -18,18 +40,24 @@ const RegisterScreen = () => {
           name="email"
           className="auth__input"
           autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           placeholder="Password"
           name="password"
           className="auth__input"
+          value={password}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           placeholder="Confirm password"
           name="password2"
           className="auth__input"
+          value={password2}
+          onChange={handleInputChange}
         />
         <button className="btn btn-primary btn-block mb-5" type="submit">
           Register
