e442c7688493cc4b71b9154c305fbca13c334b9e
ansango <anibalsantosgo@gmail.com>
2021-09-07 15:05:43 +0200

    slected

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,10 +1,13 @@
+import NothingSelected from "./NothingSelected";
 import Sidebar from "./Sidebar";
 
 const JournalScreen = () => {
   return (
     <div className="journal__main-content">
       <Sidebar />
-      <main>Main content</main>
+      <main>
+        <NothingSelected />
+      </main>
     </div>
   );
 };
