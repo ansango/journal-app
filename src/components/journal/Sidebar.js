2940f70d30ee207a547334d6bed733d3fbce4656
ansango <anibalsantosgo@gmail.com>
2021-09-07 14:59:42 +0200

    entries

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,3 +1,5 @@
+import JournalEntries from "./JournalEntries";
+
 const Sidebar = () => {
   return (
     <aside className="journal__sidebar">
@@ -14,6 +16,7 @@ const Sidebar = () => {
         <i className="far fa-calendar-plus fa-5x"></i>
         <p className="mt-5">New entry</p>
       </div>
+      <JournalEntries />
     </aside>
   );
 };
